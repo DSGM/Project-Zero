08fec7edf5e3d12c678b637beae254d3637977db
Bilal ayachi <bilal.ayachi@hetmml.nl>
2018-12-13 08:54:24 +0100

    server word gehost op port 3000

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+//Luisterd op port 8000,
+app.listen(port,(port) => {
+    console.log("Website word gehost op port " + port)
+})

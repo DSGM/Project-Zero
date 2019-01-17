82680d38178753b43b92f164c61266b59c90f9e8
Bilal ayachi <bilal.ayachi@hetmml.nl>
2019-01-17 09:11:45 +0100

    matthias center geimplemiteerd

--- public/JS/main.js
+++ public/JS/main.js
@@ -2,3 +2,16 @@ let leiden = document.getElementById('leiden').addEventListener('click',() => {
     console.log("klik")
     window.location.href = "#"
 })
+
+function centerButtons() {
+    var buttonDivWidth = $('.buttons').width();
+    var buttonDivHeight = $('.buttons').height();
+    var screenWidth = $(window).width();
+    var screenHeight = $(window).height();
+
+    var buttonPositionX = screenWidth/2 - buttonDivWidth/2;
+    var buttonPositionY = screenHeight/2 - buttonDivHeight/2;
+    $('.buttons').css({left: buttonPositionX, top: buttonPositionY});
+}
+
+setInterval(centerButtons, 100)

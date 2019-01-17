c747729c7c564d1a7d88f6df84cbb35184bc9e99
Bilal ayachi <bilal.ayachi@hetmml.nl>
2019-01-17 09:27:33 +0100

    global variablen gezet

--- public/JS/main.js
+++ public/JS/main.js
@@ -1,13 +1,18 @@
+let buttonDivWidth;
+let buttonDivHeight;
+let screenWidth;
+let screenHeight;
+
 let leiden = document.getElementById('leiden').addEventListener('click',() => {
     console.log("klik")
     window.location.href = "#"
 })
 
 function centerButtons() {
-    var buttonDivWidth = $('.buttons').width();
-    var buttonDivHeight = $('.buttons').height();
-    var screenWidth = $(window).width();
-    var screenHeight = $(window).height();
+    buttonDivWidth = $('.buttons').width();
+    buttonDivHeight = $('.buttons').height();
+    screenWidth = $(window).width();
+    screenHeight = $(window).height();
 
     var buttonPositionX = screenWidth/2 - buttonDivWidth/2;
     var buttonPositionY = screenHeight/2 - buttonDivHeight/2;

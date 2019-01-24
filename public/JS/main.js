0810ae2c7e3daa560f0ab2f3384410565aa07150
Bilal ayachi <bilal.ayachi@hetmml.nl>
2019-01-24 08:56:39 +0100

    de quote is nu random en zit in de p tag

--- public/JS/main.js
+++ public/JS/main.js
@@ -7,7 +7,7 @@ let buttonPositionY;
 
 let quotes = ["een stad voor jong een oud","Eene groene en een kleurrijke stad","Gezelegdste stad in nederland"]
 let random = Math.round(Math.random(0,2))
-console.log(quotes[random])
+let quote_tag = document.getElementById("stad").innerHTML = quotes[random];
 
 
 function centerButtons() {

bf7346e5d3e732e379371ebfdc260993932095bb
Bilal ayachi <bilal.ayachi@hetmml.nl>
2019-01-30 09:01:43 +0100

    random quote nu echt random

--- public/JS/main.js
+++ public/JS/main.js
@@ -5,20 +5,8 @@ let screenHeight;
 let buttonPositionX;
 let buttonPositionY;
 
-let quotes = ["een stad voor jong een oud","Eene groene en een kleurrijke stad","Gezelegdste stad in nederland","yeeto"]
-let random = Math.round(Math.random(0,quotes.length))
+let quotes = ["een stad voor jong een oud","Eene groene en een kleurrijke stad","Gezelegdste stad in nederland","yeeto","verander me"]
+let lengte_quote = quotes.length - 1;
+let random = Math.floor(Math.random() * lengte_quote) + 1;
+console.log(random)
 let quote_tag = document.getElementById("stad").innerHTML = quotes[random];
-
-
-// function centerButtons() {
-//     buttonDivWidth = $('.buttons').width();
-//     buttonDivHeight = $('.buttons').height();
-//     screenWidth = $(window).width();
-//     screenHeight = $(window).height();
-//
-//     buttonPositionX = screenWidth/2 - buttonDivWidth/2;
-//     buttonPositionY = screenHeight/2 - buttonDivHeight/2;
-//     $('.buttons').css({left: buttonPositionX, top: buttonPositionY});
-// }
-
-// setInterval(centerButtons, 100)
